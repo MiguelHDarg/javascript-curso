309e7608689a807fc4fea5e64084f8df7975157a
MiguelHDarg <miguelh.film@gmail.com>
2021-03-18 21:44:45 -0500

    Clase 02

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let nombre = prompt("¿Cuál es tu nombre?");
+
+alert("Hola " + nombre);
+
+let añonacimiento = prompt("¿En qué año naciste?");
+
+let añoactual = prompt("¿En qué año estamos?");
+
+let tuedad = añoactual - añonacimiento;
+
+alert("Eso quiere decir que tienes " + tuedad);
+
+let miaño = 1992;
+
+alert("Yo nací en el año " + miaño);
+
+let minombre = "Miguel";
+
+alert("Y mi nombre es " + minombre);
+
+let resultado = "Tú eres " + nombre + " y tienes " +  tuedad;
+
+alert(resultado);
+
+
+
+
+
+
